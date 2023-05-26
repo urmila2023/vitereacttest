5daa9f93dfc6ad423fc5c7af54789eb9c0f6f2b5
urmila2023 <urmila@difm.tech>
2023-05-26 07:54:53 -0400

    change

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <button @click="count++">count is: {{ count }}</button>
+  <p>Edit <code>components/urmila.vue</code> to test not module replacement.</p>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
